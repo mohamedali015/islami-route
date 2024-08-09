364cd7dcf3a82c6e2de5bce3ce6643cc770477c5
Mohamed Ali <mohamedali01537@gmail.com>
2024-08-10 01:57:23 +0300

    feat: add shared Preferences to save changes in app

--- lib/UI/Providers/locale_provider.dart
+++ lib/UI/Providers/locale_provider.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:islami/ui_utils.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LocaleProvider extends ChangeNotifier {
   String currentLocale = 'en';
+  static const String langKey = "lang";
+
+  final SharedPreferences prefs;
+
+  LocaleProvider({required this.prefs}) {
+    readSavedLocale();
+  }
+
+  void readSavedLocale() async {
+    currentLocale = prefs.getString(langKey) ?? 'en';
+  }
+
+  void saveLocale() async {
+    prefs.setString(langKey, currentLocale);
+  }
 
   void changeLocale(String newLocale) {
     currentLocale = newLocale;
     notifyListeners();
+    saveLocale();
   }
 
   static LocaleProvider get(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,17 @@ import 'package:islami/UI/Providers/theme_provider.dart';
 import 'package:islami/UI/Splash%20Screen/splash_screen.dart';
 import 'package:islami/UI/Theme/mytheme.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var sharedPreferences = await SharedPreferences.getInstance();
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
+      create: (context) => ThemeProvider(prefs: sharedPreferences),
     ),
     ChangeNotifierProvider(
-      create: (context) => LocaleProvider(),
+      create: (context) => LocaleProvider(prefs: sharedPreferences),
     ),
   ], child: const MyApp()));
 }
