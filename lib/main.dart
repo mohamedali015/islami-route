2d6175407a9e61d3ddaa3df08430d09245bd5756
Mohamed Ali <mohamedali01537@gmail.com>
2024-08-18 16:16:57 +0300

    fix: run splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         QuranDetails.routeName: (_) => const QuranDetails(),
         HadethDetails.routeName: (_) => const HadethDetails(),
       },
-      initialRoute: HomeScreen.routeName,
+      initialRoute: SplashScreen.routeName,
       theme: MyThemeData.lighttheme,
       darkTheme: MyThemeData.darktheme,
       themeMode: themeProvider.currentTheme,
